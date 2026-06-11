c9d838c714fcb793fe12a087b0676df53f9857cc
Nurtimax <maksatbeknurtilek@gmail.com>
2026-06-11 12:18:20 +0600

    remove local base url in axios interceptor

--- src/utils/helpers/axios.helper.ts
+++ src/utils/helpers/axios.helper.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8100/",
+  baseURL: "/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
