6e83ceef6c11b281f331f2ce110a4d803584c560
Nurtimax <maksatbeknurtilek@gmail.com>
2026-06-12 15:15:15 +0600

    add telegram expand

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const container = document.getElementById('root');
+declare global {
+  interface Window {
+    Telegram: {
+      WebApp: {
+        expand: () => void;
+        // add other WebApp properties you need here
+      };
+    };
+  }
+}
+
+const app = window.Telegram.WebApp;
+
+app.expand();
+
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
