9d2d48f20a635c5798994bf4ab5fbf268b435d29
Nurtimax <maksatbeknurtilek@gmail.com>
2026-06-20 04:05:21 +0600

    feat(meal): enhance MealDayAction with additional properties
    - Added properties: vegetables (string[]), ingredients (string[]), isFried (boolean), isDietary (boolean), and dietaryNote (string) to the MealDayAction interface.
    
    fix(mealCard): add optional chaining for meal name
    
    - Updated MealCard component to safely access meal name using optional chaining.
    
    chore(deps): update yarn.lock with new esbuild and rollup packages
    
    - Added multiple new esbuild and rollup packages for various architectures and platforms.

--- src/@types/meal.types.ts
+++ src/@types/meal.types.ts
@@ -3,7 +3,13 @@
 export interface MealDayAction {
   name: string;
   complete: boolean;
+  vegetables: string[];
+  ingredients: string[];
+  isFried: boolean;
+  isDietary: boolean;
+  dietaryNote: string;
 }
+
 export interface MealDay {
   day: number;
   breakfast: MealDayAction;
